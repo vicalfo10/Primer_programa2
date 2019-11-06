e56638235a4d4d1dc097f56d8da192224770393a
vgranadosr <vgranadosr@CRI-D2856>
2019-11-05 18:17:14 -0600

    VGR-05 Cambio en el Login

---
+++ Primer_Programa/Primer_Programa/Controllers/SistemaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Primer_Programa.Controllers
+{
+    public class SistemaController : Controller
+    {
+        // GET: SIstema
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
